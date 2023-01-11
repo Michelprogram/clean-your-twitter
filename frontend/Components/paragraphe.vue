4563553f00ed957a3a485415bb0b64c9639e078d
Gauron dorian <gaurondorian@gmail.com>
2023-01-11 19:18:53 +0100

    :art: Page about

---
+++ frontend/Components/paragraphe.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="paragraphe-component">
+    <h3 class="title">{{ title }}</h3>
+    <p class="text" v-html="description"></p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  title: { type: String },
+  description: { type: String },
+});
+</script>
+
+<style lang="scss" scoped>
+@forward "@/assets/scss/variables/fonts";
+
+@import "@/assets/scss/colors.scss";
+
+.paragraphe-component {
+  color: $main;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  gap: 1em;
+
+  margin: 2em;
+  .title {
+    font-family: "Faustina", serif;
+    font-size: 3vw;
+    font-weight: 500;
+    letter-spacing: 0.02em;
+  }
+
+  .text {
+    font-size: 1.5vw;
+    white-space: pre-line;
+
+    .link {
+      background-color: red;
+    }
+  }
+}
+</style>
